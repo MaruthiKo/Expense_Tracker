8f76d31846048123662228827716d60672888b93
MaruthiKo <marurohi@gmail.com>
2024-03-27 10:41:25 +0530

    Project boilerplate code added

---
+++ backend/dummyData/data.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        _id: "1",
+        username: "user1",
+        name: "User One",
+        password: "test1",
+        profilePicture: "sample.jpg",
+        gender: "Male"
+    },
+    {
+        _id: "2",
+        username: "user2",
+        name: "User Two",
+        password: "test2",
+        profilePicture: "sample2.jpg",
+        gender: "Female"
+    },
+    {
+        _id: "3",
+        username: "user3",
+        name: "User Three",
+        password: "test3",
+        profilePicture: "sample3.jpg",
+        gender: "Female"
+    }
+]
---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,40 @@
+const userTypeDef = `#graphql
+    type User {
+        _id: ID!
+        username: String!
+        name: String!
+        password: String!
+        profilePicture: String
+        gender: String!
+    }
+
+    type Query {
+        users: [User!]
+        authUser: User
+        user(userId:ID!): User 
+    }
+
+    type Mutation {
+        signUp(input: SignupInput!): User
+        login(input: LoginInput!): User
+        logout: LogOutResponse
+    }
+
+    input SignupInput {
+        username: String!
+        name: String!
+        password: String!
+        gender: String!
+    }
+
+    input LoginInput {
+        username: String!
+        password: String!
+    }
+
+    type LogOutResponse {
+        message: String!
+    }
+`
+
+export default userTypeDef;
