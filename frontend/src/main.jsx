447dbdb1c1ffb4437ab58c9fb304343f18c35dc4
MaruthiKo <marurohi@gmail.com>
2024-03-30 03:47:02 +0530

    deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   // TODO => Update the URI on production
-  uri: 'http://localhost:4000/graphql', // This is the URL of our GraphQL server
+  uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // This is the URL of our GraphQL server
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include" // This tells Apollo Client to send cookies along with every request to the server
 });
